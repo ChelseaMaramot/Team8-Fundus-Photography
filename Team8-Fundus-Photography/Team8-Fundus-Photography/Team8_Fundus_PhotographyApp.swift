9ea9d89319b3df38217e6e282750ce0262480971
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-11-03 20:54:01 -0500

    setting up swift template

---
+++ Team8-Fundus-Photography/Team8-Fundus-Photography/Team8_Fundus_PhotographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team8_Fundus_PhotographyApp.swift
+//  Team8-Fundus-Photography
+//
+//  Created by chelsea maramot on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct Team8_Fundus_PhotographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Team8-Fundus-Photography/Team8-Fundus-PhotographyTests/Team8_Fundus_PhotographyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team8_Fundus_PhotographyTests.swift
+//  Team8-Fundus-PhotographyTests
+//
+//  Created by chelsea maramot on 2024-11-03.
+//
+
+import Testing
+@testable import Team8_Fundus_Photography
+
+struct Team8_Fundus_PhotographyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

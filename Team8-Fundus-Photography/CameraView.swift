8b557e73091f2159fde51e6e653cd7b8332176fc
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-11-10 21:43:40 -0500

    #6 configuring session in camera manager

---
+++ Team8-Fundus-Photography/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  Team8-Fundus-Photography
+//
+//  Created by chelsea maramot on 2024-11-10.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
---
+++ Team8-Fundus-Photography/Team8-Fundus-Photography/Controller/CameraController.swift
@@ -0,0 +1,8 @@
+//
+//  CameraController.swift
+//  Team8-Fundus-Photography
+//
+//  Created by chelsea maramot on 2024-11-10.
+//
+
+import Foundation

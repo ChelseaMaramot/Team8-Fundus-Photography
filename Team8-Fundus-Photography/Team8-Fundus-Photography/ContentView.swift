76a31b21927d4de72899b9556302d77ab2e5aa93
Chelsea Maramot <61253723+ChelseaMaramot@users.noreply.github.com>
2025-02-03 17:33:11 -0500

    Revert "27 image crop"

--- Team8-Fundus-Photography/Team8-Fundus-Photography/ContentView.swift
+++ Team8-Fundus-Photography/Team8-Fundus-Photography/ContentView.swift
@@ -1,12 +1,16 @@
+//
+//  ContentView.swift
+//  Team8-Fundus-Photography
+//
+//  Created by chelsea maramot on 2024-11-03.
+//
+
 import SwiftUI
 
 struct ContentView: View {
     
     var body: some View {
-        ZStack {
-            CameraView()
-            QuadrantView()
-        }
+        CameraView()
     }
 }
 

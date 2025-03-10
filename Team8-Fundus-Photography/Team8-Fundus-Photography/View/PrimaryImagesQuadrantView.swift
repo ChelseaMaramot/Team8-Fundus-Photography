faf4ffb0e480910f71da9901af5e0844dc7f1a3e
Anjola-a <59450183+Anjola-a@users.noreply.github.com>
2025-03-10 02:10:37 -0400

    fixed navigation bug

--- Team8-Fundus-Photography/Team8-Fundus-Photography/View/PrimaryImagesQuadrantView.swift
+++ Team8-Fundus-Photography/Team8-Fundus-Photography/View/PrimaryImagesQuadrantView.swift
@@ -3,30 +3,30 @@
 //  Team8-Fundus-Photography
 //
 //  Created by Anjola Adewale on 2025-03-09.
-//
+
+
 
 import SwiftUI
-
-
 
 struct PrimaryImagesQuadrantView: View {
     let images: [String: UIImage] // Dictionary of quadrant names to images
     
     var body: some View {
         ZStack {
-
             // Position each image in its quadrant
             ForEach(["Superior", "Inferior", "Nasal", "Temporal", "Central"], id: \.self) { quadrant in
                 if let image = images[quadrant] {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90) // Adjust size as needed
-                        .clipShape(Circle()) // Ensure images are circular
-                        .overlay(
-                            Circle().stroke(Color.blue, lineWidth: 2) // Border to indicate active scan
-                        )
-                        .offset(offsetForQuadrant(quadrant)) // Position image correctly
+                    NavigationLink(destination: ImageView(image: image)) {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 90, height: 90) // Adjust size as needed
+                            .clipShape(Circle()) // Circular images
+                            .overlay(
+                                Circle().stroke(Color.blue, lineWidth: 2) // Border for primary image
+                            )
+                    }
+                    .offset(offsetForQuadrant(quadrant)) // Position correctly
                 } else {
                     // Placeholder if no image exists for this quadrant
                     Circle()
